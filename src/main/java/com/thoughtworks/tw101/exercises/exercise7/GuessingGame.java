ecabe663cb613db3e536b9eaad7b0ec740374c0d
Rebecca Eakins <rebecca.a.eakins@gmail.com>
2015-12-16 11:42:25 -0500

    completes ex 7, mvp number guesser

---
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/GuessingGame.java
@@ -0,0 +1,62 @@
+package com.thoughtworks.tw101.exercises.exercise7;
+
+public class GuessingGame {
+    private int secretNumber;
+    private boolean isGuessed;
+    private GameHelper helper = new GameHelper();
+
+    public GuessingGame(int maxBound) {
+        secretNumber = (int) (Math.random() * maxBound);
+        System.out.println("FYI, the secret number is: " + secretNumber);
+    }
+
+
+    public void layGroundRules() {
+        notify(border());
+        notify("Welcome to Guess That Number!");
+        notify("Try to guess a randomly generated number, between 1 and 100.");
+        notify("PSA: Please only use digits in your guess. For example '79' not 'seventy-nine'.");
+        notify(border());
+    }
+
+    public void takeGuesses() {
+
+        while(!isGuessed) {
+            String guess = helper.getUserInput("Please enter a number guess: ");
+            checkGuess(guess);
+        }
+
+        finishUp();
+    }
+
+    public void checkGuess(String guess){
+
+        int num = Integer.parseInt(guess);
+
+        if (num == secretNumber) {
+            isGuessed = true;
+            notify("oooOOOoo!");
+        } else if (num < secretNumber) {
+            notify("Too low!");
+        } else if (num > secretNumber) {
+            notify("Too high!");
+        } else {
+            notify("Erm....dunno what to do with that...");
+        }
+    }
+
+    public void finishUp() {
+        notify("Well done! You guessed the secret number, " + secretNumber + ".");
+    }
+
+    private void notify(String message){
+        System.out.println(message);
+    }
+
+    private String border() {
+        return "%%%%%%%%%%%%%%%%%%%%";
+    }
+
+
+
+}
--- src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.tw101.exercises.exercise7;
 
+
 // Write a program that chooses a random number between 1 and 100. Then asks the user to enter a guess. If they guess
 // right then tell them they win, otherwise tell them if they guessed too high or low. Keep asking the user to guess
 // until they get the right answer. Use classes to separate the different concerns of this program.
@@ -7,6 +8,8 @@ package com.thoughtworks.tw101.exercises.exercise7;
 public class Main {
 
     public static void main(String[] args) {
-
+        GuessingGame game = new GuessingGame(100);
+        game.layGroundRules();
+        game.takeGuesses();
     }
 }
