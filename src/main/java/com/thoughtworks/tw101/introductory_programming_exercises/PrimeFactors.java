28e47295fb358d8b13ee3f39145f7bb01a9c367a
Rebecca Eakins <rebecca.a.eakins@gmail.com>
2015-12-21 14:41:44 -0600

    completes first iteration of prime factors

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,14 +6,32 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
     public static void main(String[] args) {
-        List<Integer> primeFactors = generate(30);
+        List<Integer> primeFactors = generate(10);
+
+        for( int factor : primeFactors){
+            System.out.println(factor);
+        }
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+
+        List<Integer> factors = new ArrayList<>();
+        int divisor = 2;
+
+        while (n > 1) {
+            if (n % divisor == 0){
+                factors.add(divisor);
+                n /= divisor;
+            } else {
+                divisor += 1;
+            }
+        }
+
+        return factors;
     }
 }
