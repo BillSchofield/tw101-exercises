0aeb708a22a9253b556ec64b4d4ceb1f3eeb2a83
Rebecca Eakins <rebecca.a.eakins@gmail.com>
2015-12-22 13:36:09 -0600

    repetitve but all diamond exercises work

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -2,34 +2,41 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 
 public class DiamondExercises {
     public static void main(String[] args) {
-//        drawAnIsoscelesTriangle(3);
-//        drawADiamond(8);
+        drawAnIsoscelesTriangle(3);
+        drawADiamond(8);
         drawADiamondWithYourName(3);
     }
 
-//    Isosceles Triangle
+    //    Isosceles Triangle
 //    Given a number n, print a centered triangle. Example for n=3:
 //              *
 //             ***
 //            *****
     private static void drawAnIsoscelesTriangle(int n) {
-        int width = (2*n - 1);
+        int width = (2 * n - 1);
+        drawAnIsoscelesTriangle(n, width);
+    }
 
-        for(int i = 1; i <= n; i++){
+    private static void drawAnIsoscelesTriangle(int n, int width) {
+        for (int i = 1; i <= n; i++) {
             System.out.println(centeredRow(starsForRow(i), width));
         }
     }
 
     private static void drawUpsideDownIsoscelesTriangle(int n){
-        int offset = n - 1;
-        int width = (2*n - 1);
+        int width = (2 * n - 1);
+        drawUpsideDownIsoscelesTriangle(n, width);
+    }
 
-        for(int i = offset; i > 0; i--){
+    private static void drawUpsideDownIsoscelesTriangle(int n, int width) {
+        int offset = n - 1;
+
+        for (int i = offset; i > 0; i--) {
             System.out.println(centeredRow(starsForRow(i), width));
         }
     }
 
-//    Diamond
+    //    Diamond
 //    Given a number n, print a centered diamond. Example for n=3:
 //              *
 //             ***
@@ -41,7 +48,7 @@ public class DiamondExercises {
         drawUpsideDownIsoscelesTriangle(n);
     }
 
-//    Diamond with Name
+    //    Diamond with Name
 //    Given a number n, print a centered diamond with your name in place of the middle line. Example for n=3:
 //
 //             *
@@ -50,15 +57,17 @@ public class DiamondExercises {
 //            ***
 //             *
     private static void drawADiamondWithYourName(int n) {
-        drawAnIsoscelesTriangle(n-1);
-        System.out.println("Rebecca");
-        drawUpsideDownIsoscelesTriangle(n);
+        String name = "Rebecca";
+
+        drawAnIsoscelesTriangle(n-1, name.length());
+        System.out.println(name);
+        drawUpsideDownIsoscelesTriangle(n, name.length());
 
     }
 
     private static String starsForRow(int n) {
         StringBuilder sb = new StringBuilder("");
-        for(int i = 0; i < (2*n - 1); i++){
+        for (int i = 0; i < (2 * n - 1); i++) {
             sb.append("*");
         }
 
@@ -76,7 +85,7 @@ public class DiamondExercises {
         } else {
             int sizeWithRightPadding = stars.length() + (padding / 2);
             String formattedString = String.format("%" + sizeWithRightPadding + "s", stars);
-            return  formattedString;
+            return formattedString;
         }
 
     }
