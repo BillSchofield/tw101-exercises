87ca59caac30e4d2dd2f6ca47e81c5aefdc07d91
lingtran <lxiutran@gmail.com>
2016-12-08 20:04:48 -0600

    Complete exercise 1
    Initial implementation for exercise 1

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -4,6 +4,14 @@ package com.thoughtworks.tw101.exercises.exercise1;
 
 public class Main {
     public static void main(String[] args) {
+        int sum = 0;
 
+        for (int i = 0; i < 100; i++) {
+            if ( i % 2 != 0 ) {
+                sum += i;
+                System.out.println(i);
+            }
+        }
+        System.out.println("The sum of all these odd numbers is " + sum);
     }
 }
