72d722f1b9e6692648ead4196bdceed36173f292
MsLarissaaa <larissapittenger@gmail.com>
2017-01-01 16:02:49 -0600

    Commit solution to first exercise - print all the odd numbers from 1 to 100 and their sum.

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -4,6 +4,15 @@ package com.thoughtworks.tw101.exercises.exercise1;
 
 public class Main {
     public static void main(String[] args) {
-
+        int sum = 0;
+        int number = 1;
+        while (number <= 100){
+            if (number % 2 == 1){
+                sum += number;
+                System.out.println(number);
+            }
+            number++;
+        }
+        System.out.println("The sum total of all odd numbers from 1 to 100 is " + sum);
     }
 }
