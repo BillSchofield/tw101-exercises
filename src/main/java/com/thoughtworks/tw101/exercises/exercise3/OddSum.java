3c077e17a098aad2489ea1de2ded0d3c59d4b673
Brian Sayler <sayler.b@gmail.com>
2017-03-15 23:55:49 -0600

    add ex3

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,14 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        int sum = 0;
+
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 1) {
+                sum += i;
+            }
+
+        }
+        return sum;
     }
 }
