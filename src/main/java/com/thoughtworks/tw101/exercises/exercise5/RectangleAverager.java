3bf3d09215d3cccc973110e4cc459bb9de0a0dd4
Brian Sayler <sayler.b@gmail.com>
2017-03-19 22:58:55 -0600

    add ex5

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -3,6 +3,12 @@ package com.thoughtworks.tw101.exercises.exercise5;
 public class RectangleAverager {
 
     public float averageArea(Rectangle[] rectangles) {
-        return 0;
+        int sum = 0;
+        for (Rectangle rectangle : rectangles) {
+            sum += rectangle.area();
+        }
+
+        return sum / rectangles.length;
     }
+
 }
