d20174eaef6a574e90fdabaa8d6462c2411014c7
Brian Sayler <sayler.b@gmail.com>
2017-03-19 23:03:54 -0600

    upgrade to java 8 and use stream

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -1,12 +1,11 @@
 package com.thoughtworks.tw101.exercises.exercise5;
 
+import java.util.Arrays;
+
 public class RectangleAverager {
 
     public float averageArea(Rectangle[] rectangles) {
-        int sum = 0;
-        for (Rectangle rectangle : rectangles) {
-            sum += rectangle.area();
-        }
+        int sum = Arrays.stream(rectangles).mapToInt(r -> r.area()).sum();
 
         return sum / rectangles.length;
     }
