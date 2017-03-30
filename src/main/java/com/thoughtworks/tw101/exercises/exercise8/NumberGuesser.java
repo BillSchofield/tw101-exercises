e414f3dbd2b7bff990747805a7cc26c3dd4a1334
Brian Sayler <sayler.b@gmail.com>
2017-03-30 17:22:35 -0600

    record guesses and add error handling
    - added instance variable to track guesses
    - aded a try/catch block to handle string input

--- src/main/java/com/thoughtworks/tw101/exercises/exercise8/NumberGuesser.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise8/NumberGuesser.java
@@ -1,15 +1,19 @@
 package com.thoughtworks.tw101.exercises.exercise8;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class NumberGuesser {
     private int secretNumber;
     private int currentGuess;
+    private List<Integer> guesses;
 
     public NumberGuesser() {
         secretNumber = (int) (Math.random() * 100 + 1);
         currentGuess = -1;
-
+        guesses = new ArrayList<Integer>();
     }
 
     public void play() {
@@ -17,10 +21,20 @@ public class NumberGuesser {
 
         do {
             System.out.print(Messages.requestGuess());
-            currentGuess = input.nextInt();
-            checkGuess(currentGuess);
+
+            try {
+                currentGuess = input.nextInt();
+                guesses.add(currentGuess);
+                checkGuess(currentGuess);
+            } catch (InputMismatchException error) {
+                currentGuess = -1;
+                System.out.println("Sorry, that's not a valid guess");
+                input.next();
+            }
+
         } while (currentGuess != secretNumber);
 
+        System.out.println("Your guesses: " + guesses);
     }
 
     private void checkGuess(int currentGuess) {
