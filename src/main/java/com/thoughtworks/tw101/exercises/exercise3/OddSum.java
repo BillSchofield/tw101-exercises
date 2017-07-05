5aced780b5ceaaac15ed9756f1b68a4c4bb317dd
cgrad01 <cgrad01@gmail.com>
2017-07-05 15:29:14 -0400

    make setter and instance variables private

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -1,10 +1,10 @@
 package com.thoughtworks.tw101.exercises.exercise3;
 
 public class OddSum {
-    int adjuster = 0;
-    int sum = 0;
+    private int adjuster = 0;
+    private int sum = 0;
 
-    void setAdjuster(int start) {
+    private void setAdjuster(int start) {
         if (start%2 == 0){
             adjuster = 1;
         }
