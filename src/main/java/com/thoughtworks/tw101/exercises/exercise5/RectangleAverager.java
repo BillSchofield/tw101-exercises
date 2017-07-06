b0d741fa80b68b6665bb20742dc07933d926f28d
cgrad01 <cgrad01@gmail.com>
2017-07-06 15:29:08 -0400

    print average area

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -3,6 +3,10 @@ package com.thoughtworks.tw101.exercises.exercise5;
 public class RectangleAverager {
 
     public float averageArea(Rectangle[] rectangles) {
-        return 0;
+        int total = 0;
+        for(Rectangle rectangle : rectangles){
+            total += rectangle.area();
+        }
+        return total/rectangles.length;
     }
 }
