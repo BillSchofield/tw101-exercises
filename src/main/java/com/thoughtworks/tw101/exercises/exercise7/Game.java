fac3309f7e9273f53f25d6b1583c860e48dad223
cgrad01 <cgrad01@gmail.com>
2017-07-11 10:49:00 -0400

    give high/low feedback

--- src/main/java/com/thoughtworks/tw101/exercises/exercise7/Game.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/Game.java
@@ -1,27 +1,28 @@
 package com.thoughtworks.tw101.exercises.exercise7;
 
+import java.util.Random;
+
 /**
  * Created by cgrad01 on 7/11/17.
  */
 public class Game {
 
-    private int number;
-    private Player user;
+    private final int NUMBER = new Random().nextInt(100)+1;
+    private Player player;
 
-    public Game(Player user) {
-        this.user = user;
+    public Game(Player player) {
+        this.player = player;
     }
 
-    private void setNumber(){
-         number = (int) (Math.random() * 100);
+    private void evaluateGuess(int guess){
+        System.out.println((NUMBER < guess ? "Too high" : "Too low"));
     }
 
     public void play() {
-        setNumber();
-        System.out.println(number);
-        while (number != user.getNumber()) {
-            System.out.println("Guess again!");
-            user.guessNumber();
+        player.guessNumber();
+        while (NUMBER != player.getNumber()) {
+            evaluateGuess(player.getNumber());
+            player.guessNumber();
         }
         System.out.println("You win!");
     }
