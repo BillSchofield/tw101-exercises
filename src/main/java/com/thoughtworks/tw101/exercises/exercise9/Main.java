717a8accb2415538bc480a126014e25f229851e1
cgrad01 <cgrad01@gmail.com>
2017-07-18 08:48:19 -0400

    add left and right nodes

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Main.java
@@ -2,9 +2,9 @@ package com.thoughtworks.tw101.exercises.exercise9;
 
 import java.util.List;
 
-// Exercise #9: Create a class Node that implements a binary tree for Strings. It should have an add(String name) method
-// that recursively finds the correct place to add a new node and a names() method that returns the names of all of the
-// nodes in alphabetical order (this should also be implemented using recursion).
+// Exercise #9: Create a class Node that implements a binary tree for Strings.
+// It should have an add(String name) method that recursively finds the correct place to add a new node
+// and a names() method that returns the names of all of the nodes in alphabetical order (this should also be implemented using recursion).
 
 public class Main {
     public static void main(String[] args) {
@@ -23,5 +23,6 @@ public class Main {
         for (String name : names) {
             System.out.println(name);
         }
+
     }
 }
--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -4,15 +4,35 @@ import java.util.List;
 
 public class Node {
     private String name;
+    private Node left;
+    private Node right;
+    private Node parent;
 
     public Node(String name) {
         this.name = name;
     }
 
     public void add(String nameOfNewNode) {
+        Node newNode = new Node(nameOfNewNode);
+        if(name.compareTo(newNode.name) >= 1 && left == null){
+            left = newNode;
+        }
+        else if(name.compareTo(newNode.name) <= -1 && right == null){
+            right = newNode;
+        }
     }
 
     public List<String> names() {
         return null;
     }
+
+    public static void main(String[] args){
+        Node root = new Node("Cecil");
+        root.add("Michelle");
+        root.add("Bill");
+
+        System.out.println(root.left.name);
+        System.out.println(root.right.name);
+    }
+
 }
