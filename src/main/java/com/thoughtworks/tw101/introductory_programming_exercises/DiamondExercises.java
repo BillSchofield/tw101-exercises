c1962a0d8ada61e205c2e7ee26f1da4da09fe018
cgrad01 <cgrad01@gmail.com>
2017-07-18 13:45:10 -0400

    print 3 triangle

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -1,10 +1,13 @@
 package com.thoughtworks.tw101.introductory_programming_exercises;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class DiamondExercises {
     public static void main(String[] args) {
         drawAnIsoscelesTriangle(3);
-        drawADiamond(8);
-        drawADiamondWithYourName(3);
+//        drawADiamond(8);
+//        drawADiamondWithYourName(3);
     }
 
 //    Isosceles Triangle
@@ -12,9 +15,32 @@ public class DiamondExercises {
 //              *
 //             ***
 //            *****
+//           *******
+//          *********
     private static void drawAnIsoscelesTriangle(int n) {
+        for(int i=0; i<=n+1; i+=2){
+            for (int j = 1; j<i+2; j++){
+                if(j==1){
+                    for(int k = 0; k<(n-i)+1; k+=2){
+                        System.out.print(" ");
+                    }
+                }
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
 
-    }
+
+
+//    for(String printLine :lines){
+//            for(int i = 0; i<lines.indexOf(printLine)+(lines.size()); i+=2){
+//        printLine = " " + printLine;
+//
+//    }
+//            System.out.println(lines.indexOf(printLine));
+//            System.out.println(printLine);
+//}
 
 //    Diamond
 //    Given a number n, print a centered diamond. Example for n=3:
