10b062259a905101b4d7c6d5e23564d100171cc6
cgrad01 <cgrad01@gmail.com>
2017-07-18 14:48:35 -0400

    print diamonds with name

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -1,14 +1,10 @@
 package com.thoughtworks.tw101.introductory_programming_exercises;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 public class DiamondExercises {
     public static void main(String[] args) {
-//        drawAnIsoscelesTriangle(3);
-        drawADiamond(7);
+        drawAnIsoscelesTriangle(3);
         drawADiamond(8);
-//        drawADiamondWithYourName(3);
+        drawADiamondWithYourName(4);
     }
 
 //    Isosceles Triangle
@@ -32,16 +28,7 @@ public class DiamondExercises {
         }
     }
 
-
-//    Diamond
-//    Given a number n, print a centered diamond. Example for n=3:
-//              *
-//             ***
-//            *****
-//             ***
-//              *
-    private static void drawADiamond(int n) {
-        drawAnIsoscelesTriangle(n);
+    private static void drawAnUpsideDownTriangle(int n){
         for(int i=0; i < n+2; i+=2){
             for(int j = 1; j<=n-i+1-n%2; j++){
                 if(j==1) {
@@ -53,7 +40,19 @@ public class DiamondExercises {
             }
             System.out.println();
         }
+    }
 
+
+//    Diamond
+//    Given a number n, print a centered diamond. Example for n=3:
+//              *
+//             ***
+//            *****
+//             ***
+//              *
+    private static void drawADiamond(int n) {
+        drawAnIsoscelesTriangle(n);
+        drawAnUpsideDownTriangle(n);
     }
 
 //    Diamond with Name
@@ -65,17 +64,23 @@ public class DiamondExercises {
 //            ***
 //             *
     private static void drawADiamondWithYourName(int n) {
-        drawAnIsoscelesTriangle(n);
-        for(int i=0; i < n+2; i+=2){
-            for(int j = 1; j<=n-i+1; j++){
-                if(j==1) {
-                    for (int k = 0; k <= i; k += 2) {
+        for(int i=0; i<=n+2; i+=2){
+            for (int j = 1; j<i+2; j++){
+                if(j==1){
+                    for(int k = 0; k<(n-i)+1; k+=2){
                         System.out.print(" ");
                     }
                 }
-                System.out.print("*");
+                if(i==n+2-n%2){
+                    System.out.print("Corey");
+                    break;
+                } else {
+                    System.out.print("*");
+                }
+
             }
             System.out.println();
         }
+        drawAnUpsideDownTriangle(n);
     }
 }
