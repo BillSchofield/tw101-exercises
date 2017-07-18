0c31517f1d5e591ab18ec8500538ba3ae544d0ee
cgrad01 <cgrad01@gmail.com>
2017-07-18 16:35:36 -0400

    print fizzBuzz

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
@@ -32,6 +32,18 @@ public class FizzBuzz {
     }
 
     private static void fizzBuzz() {
-
+        for (int i = 1; i<=100; i++){
+            if(i%3==0 && i%5==0){
+                System.out.println("FizzBuzz");
+            }
+            else if(i%3==0){
+                System.out.println("Fizz");
+            }
+            else if(i%5==0){
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
