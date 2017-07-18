b147c8da9926577eb5e9d4660423f6228f042b41
cgrad01 <cgrad01@gmail.com>
2017-07-18 14:35:48 -0400

    fixed issue with odd vs even diamonds

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class DiamondExercises {
     public static void main(String[] args) {
 //        drawAnIsoscelesTriangle(3);
+        drawADiamond(7);
         drawADiamond(8);
 //        drawADiamondWithYourName(3);
     }
@@ -42,7 +43,7 @@ public class DiamondExercises {
     private static void drawADiamond(int n) {
         drawAnIsoscelesTriangle(n);
         for(int i=0; i < n+2; i+=2){
-            for(int j = 1; j<=n-i+1; j++){
+            for(int j = 1; j<=n-i+1-n%2; j++){
                 if(j==1) {
                     for (int k = 0; k <= i; k += 2) {
                         System.out.print(" ");
@@ -64,6 +65,17 @@ public class DiamondExercises {
 //            ***
 //             *
     private static void drawADiamondWithYourName(int n) {
-
+        drawAnIsoscelesTriangle(n);
+        for(int i=0; i < n+2; i+=2){
+            for(int j = 1; j<=n-i+1; j++){
+                if(j==1) {
+                    for (int k = 0; k <= i; k += 2) {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
