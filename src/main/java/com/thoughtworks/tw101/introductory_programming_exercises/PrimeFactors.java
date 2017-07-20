f68875bc1807a794e75b9c6cdb742dc57835e5a5
cgrad01 <cgrad01@gmail.com>
2017-07-20 09:34:21 -0400

    reduce factors one iteration

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,14 +6,45 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
     public static void main(String[] args) {
         List<Integer> primeFactors = generate(30);
+        List<Integer> primeFactors2 = generate(34);
+        List<Integer> primeFactors3 = generate(36);
+        List<Integer> primeFactors4 = generate(5);
+        List<Integer> primeFactors5 = generate(2);
+        System.out.println(primeFactors);
+        System.out.println(primeFactors2);
+        System.out.println(primeFactors3);
+        System.out.println(primeFactors4);
+        System.out.println(primeFactors5);
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+        ArrayList<Integer> factors = getFactors(n);
+        ArrayList<Integer> results = new ArrayList<>(factors);
+        for(int factor : factors){
+            ArrayList<Integer> moreFactors = getFactors(factor);
+            if(moreFactors.size() > 0){
+                results.remove(factors.indexOf(factor));
+                results.addAll(moreFactors);
+            }
+        }
+        return results;
     }
+
+    private static ArrayList<Integer> getFactors(int n){
+        ArrayList<Integer> factors = new ArrayList<>();
+        for(int i = 2; i < n; i++){
+            if(n%i==0){
+                factors.add(i);
+            }
+        }
+        return factors;
+    }
+
 }
